ba4936d9e7581fc7796df50c8d86337849d20690
Luke Tillman <luke.tillman@gmail.com>
2016-08-31 13:58:06 -0600

    Placeholders for subscribing to messages

--- src/common/message-bus.js
+++ src/common/message-bus.js
@@ -4,7 +4,7 @@ import util from 'util';
 
 /**
  * Publishes an event to the message bus and returns a Promise that is resolved
- * when the publish is complete.
+ * when the publish is complete. The event object should be a Protobuf message.
  */
 export function publishAsync(event) {
   // Get the Protobuf short message name
@@ -17,8 +17,26 @@ export function publishAsync(event) {
 
 /**
  * Publishes an event to the message bus and invokes the specified callback when
- * complete.
+ * complete. The event object should be a Protobuf message.
  */
 export function publish(event, cb) {
   publishAsync(event).asCallback(cb);
+};
+
+/**
+ * Subscribes the specified handler to the specified event type on the message bus. 
+ * Returns a Promise that is resolved when subscribe is complete. The eventType
+ * should be a Protobuf message definition.
+ */
+export function subscribeAsync(eventType, handler) {
+  return Promise.resolve();
+};
+
+/**
+ * Subscribes the specified handler to the specified event type on the message bus and
+ * invokes the callback when complete. The eventType should be a Profobuf message
+ * definition.
+ */
+export function subscribe(eventType, handler, cb) {
+  subscribeAsync(eventType, handler).asCallback(cb);
 };
