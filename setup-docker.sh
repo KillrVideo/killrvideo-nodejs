87006fba7073a3846decbee011fa86b6d86aff3b
Luke Tillman <luke.tillman@gmail.com>
2016-09-01 11:23:05 -0600

    Clean up shell script

--- setup-docker.sh
+++ setup-docker.sh
@@ -3,13 +3,14 @@
 set -e  # Bail if something fails
 
 # Create the .env file
-(exec ./lib/killrvideo-docker-common/create-environment.sh)
+echo 'Creating docker .env file (this may take a minute)'
+./lib/killrvideo-docker-common/create-environment.sh
 
 echo
-echo Pulling all docker dependencies 
+echo 'Pulling all docker dependencies' 
 
 # Pull all docker dependencies
 docker-compose pull
 
 echo
-echo You can now start docker dependencies with \'docker-compose up -d\'
+echo "You can now start docker dependencies with \'docker-compose up -d\'"
