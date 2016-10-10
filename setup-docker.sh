e894627bcebcd6f273811682362a0244420732de
Luke Tillman <luke.tillman@gmail.com>
2016-10-09 18:37:42 -0600

    Docker for Mac setup should create loopback aliases\

--- setup-docker.sh
+++ setup-docker.sh
@@ -6,6 +6,16 @@ set -e  # Bail if something fails
 echo 'Creating docker .env file (this may take a minute)'
 ./lib/killrvideo-docker-common/create-environment.sh
 
+# Load the .env file we just created so we have access to variables
+source ./.env
+
+# Create the loopback aliases for the two IPs in the .env file
+echo
+echo 'We need to create two aliases for the loopback adapter using sudo'
+echo 'You will be prompted for your password'
+sudo ifconfig lo0 alias $KILLRVIDEO_HOST_IP
+sudo ifconfig lo0 alias $KILLRVIDEO_DOCKER_IP
+
 echo
 echo 'Pulling all docker dependencies' 
 
