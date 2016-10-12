e67674e23cb105c06f3a6d7caeb5c08b6c7dd5c9
Luke Tillman <luke.tillman@gmail.com>
2016-10-12 10:31:21 -0600

    Remove unnecessary cassandra init code
    - Bootstrapping of schema is done in docker now

--- src/common/cassandra.js
+++ src/common/cassandra.js
@@ -1,20 +1,8 @@
-import { resolve } from 'path';
-import { createReadStream } from 'fs';
-import { createInterface } from 'readline';
 import Promise from 'bluebird';
 import { Client, types as CassandraTypes } from 'cassandra-driver';
 import { lookupServiceAsync } from './service-discovery';
 import { withRetries } from './with-retries';
 import { logger } from './logging';
-
-// The schema.cql file is copied under the data folder as part of the build scripts (see
-// the package.json file for the script command)
-const SCHEMA_FILE_PATH = resolve(__dirname, '../data/schema.cql');
-
-// The CQL to create the keyspace if it doesn't exist
-const CREATE_KEYSPACE_CQL = `
-  CREATE KEYSPACE IF NOT EXISTS killrvideo 
-  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }`;
 
 /**
  * Looks up the location of the cassandra service and creates a client with the options/keyspace 
@@ -27,44 +15,6 @@ function createClientAsync(keyspace, queryOptions) {
       Promise.promisifyAll(client); // This creates "Async" versions of methods that return promises
       return client;
     });
-}
-
-/**
- * Reads and parses statements from the schema file and returns a Promise that resolves to the array 
- * of the statements found.
- */
-function readSchemaStatementsAsync() {
-  return new Promise((resolve, reject) => {
-    // Use node's readline module
-    let reader = createInterface({
-      input: createReadStream(SCHEMA_FILE_PATH)
-    });
-
-    // Keep track of the current statement and all statements we've seen
-    let currentStatement = '';
-    let statements = [];
-
-    // On each line, do some really simple parsing
-    reader.on('line', line => {
-      // Skip blank lines
-      if (line === null || line === '') return;
-
-      // Skip comments
-      if (line.startsWith('//')) return;
-
-      // Add line to current statement
-      currentStatement += line;
-
-      // Is this the end of the statement?
-      if (line.trim().endsWith(';')) {
-        statements.push(currentStatement);
-        currentStatement = '';
-      }
-    });
-
-    // When finished, resolve the promise with all statements
-    reader.on('close', () => resolve(statements));
-  });
 }
 
 // A singleton client instance to be reused throughout the application
@@ -89,34 +39,18 @@ export function getCassandraClient() {
 export function initCassandraAsync() {
   logger.log('info', 'Initializing cassandra');
 
-  // Create the keyspace, then a client connected to the keyspace
-  let createClient = createClientAsync()
+  // Create the client with some default query options
+  return createClientAsync('killrvideo', {
+      prepare: true,
+      consistency: CassandraTypes.consistencies.localQuorum
+    })
     .tap(client => {
       // Wait until Cassandra is ready and we can connect (could be delayed if starting up for 1st time)
       return withRetries(() => client.connectAsync(), 10, 10, 'Error connecting to cassandra', false);
     })
-    .then(client => {
-      // Create the keyspace if it doesn't exist
-      return client.executeAsync(CREATE_KEYSPACE_CQL);
-    })
-    .then(() => {
-      // Now create the client we'll reuse everywhere connected to the keyspace
-      return createClientAsync('killrvideo', {
-        prepare: true,
-        consistency: CassandraTypes.consistencies.localQuorum
-      });
-    });
-
-  // Read the schema from the schema file
-  let readSchema = readSchemaStatementsAsync();
-
-  // Once we have keyspace, client for keyspace, and schema contents...
-  return Promise.join(createClient, readSchema, (client, schemaStatements) => {
-      // Save client instance for reuse everywhere
+    .tap(client => {
+      // Save client instance for reuse everywhere and log
       clientInstance = client;
-
-      // Execute the statements in order, waiting for each one to complete before doing the next one
-      return Promise.mapSeries(schemaStatements, statement => client.executeAsync(statement));
-    })
-    .tap(() => logger.log('info', 'Cassandra initialized'));
+      logger.log('info', 'Cassandra initialized')
+    });
 };
